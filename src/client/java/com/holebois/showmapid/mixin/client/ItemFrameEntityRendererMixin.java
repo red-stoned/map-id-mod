2d48e31c60e64d29d41dec0b139e31c5c45d3b67
galiano <piz@redstoned.dev>
2026-04-06 01:56:33 -0500

    26.1

--- src/client/java/com/holebois/showmapid/mixin/client/ItemFrameEntityRendererMixin.java
+++ src/client/java/com/holebois/showmapid/mixin/client/ItemFrameEntityRendererMixin.java
@@ -1,47 +1,45 @@
 package com.holebois.showmapid.mixin.client;
 
-import net.minecraft.client.MinecraftClient;
-
-import net.minecraft.component.DataComponentTypes;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.ItemFrameRenderer;
+import net.minecraft.client.renderer.entity.state.ItemFrameRenderState;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.decoration.ItemFrame;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
-import net.minecraft.client.render.entity.state.ItemFrameEntityRenderState;
-import net.minecraft.entity.decoration.ItemFrameEntity;
-import net.minecraft.text.Text;
 
-
-@Mixin(ItemFrameEntityRenderer.class)
-public abstract class ItemFrameEntityRendererMixin<T extends ItemFrameEntity>
-extends EntityRenderer<T, ItemFrameEntityRenderState> {
-	protected ItemFrameEntityRendererMixin(EntityRendererFactory.Context ctx) {
+@Mixin(ItemFrameRenderer.class)
+public abstract class ItemFrameEntityRendererMixin<T extends ItemFrame>
+extends EntityRenderer<T, ItemFrameRenderState> {
+	protected ItemFrameEntityRendererMixin(EntityRendererProvider.Context ctx) {
 		super(ctx);
 	}
 
-    @Inject(at = @At("HEAD"), method = "hasLabel", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "shouldShowName", cancellable = true)
     public void label(T itemFrameEntity, double d, CallbackInfoReturnable<Boolean> cir) {
-        Boolean show =  MinecraftClient.isHudEnabled() && 
-            !itemFrameEntity.getHeldItemStack().isEmpty() && 
-            this.dispatcher.targetedEntity == itemFrameEntity &&
+        Boolean show =  Minecraft.renderNames() && 
+            !itemFrameEntity.getItem().isEmpty() && 
+            this.entityRenderDispatcher.crosshairPickEntity == itemFrameEntity &&
             (
-                itemFrameEntity.getHeldItemStack().contains(DataComponentTypes.CUSTOM_NAME) ||
+                itemFrameEntity.getItem().has(DataComponents.CUSTOM_NAME) ||
                 (
-                    itemFrameEntity.getHeldItemStack().contains(DataComponentTypes.MAP_ID) &&
-                    MinecraftClient.getInstance().player.isSneaking()
+                    itemFrameEntity.getItem().has(DataComponents.MAP_ID) &&
+                    Minecraft.getInstance().player.isShiftKeyDown()
                 )
             ); 
         cir.setReturnValue(show);
     }
 
-    @Inject(at = @At("HEAD"), method = "getDisplayName", cancellable = true)
-    public void name(T itemFrameEntity, CallbackInfoReturnable<Text> cir) {
-        if (itemFrameEntity.getHeldItemStack().contains(DataComponentTypes.MAP_ID) && MinecraftClient.getInstance().player.isSneaking()) {
-            cir.setReturnValue(itemFrameEntity.getHeldItemStack().getName().copy().append(" <ID:" + itemFrameEntity.getHeldItemStack().get(DataComponentTypes.MAP_ID).id() + ">"));
+    @Inject(at = @At("HEAD"), method = "getNameTag", cancellable = true)
+    public void name(T itemFrameEntity, CallbackInfoReturnable<Component> cir) {
+        if (itemFrameEntity.getItem().has(DataComponents.MAP_ID) && Minecraft.getInstance().player.isShiftKeyDown()) {
+            cir.setReturnValue(itemFrameEntity.getItem().getHoverName().copy().append(" <ID:" + itemFrameEntity.getItem().get(DataComponents.MAP_ID).id() + ">"));
         }
      }
 }
